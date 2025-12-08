34e3f9dbad568236fe34635edcaa166ba75df4a6
I-Hua24 <iris_huang0129@hotmail.com>
2025-12-07 22:33:57 -0500

    conform server.js to render.com hosting

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const http = require('http');
 
+// Create server
 const server = http.createServer((req, res) => {
+  // Serve index.html for all requests
+  const filePath = path.join(__dirname, 'index.html');
 
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading page');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+// Listen on the port Render assigns
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
